e0a824af24e32b506613c7a5011c63cb85717c98
mark-marshall <mark.marshallgp@gmail.com>
2019-05-27 10:15:29 +0100

    Tidied up bignum

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,5 +1,6 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
 
-print(pow(2, 65536))
+bignum = pow(2, 65536)
+print(bignum)
 # equivalent to 2**65536
